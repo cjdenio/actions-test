a90dacf6345e777b501a49c1068d220331831c66
Caleb Denio <cjdenio44@gmail.com>
2020-07-14 19:41:33 -0400

    Testing

--- .github/actions/config-test/index.js
+++ .github/actions/config-test/index.js
@@ -4,9 +4,7 @@ const { promisify } = require('util')
 const path = require('path')
 
 async function run() {
-    const writeFile = promisify(fs.writeFile)
-
-    await writeFile(path.join(process.env.GITHUB_WORKSPACE, 'README.md'), new Date().toString())
+    console.log(process.env.INPUT_TEST)
 }
 
 run()

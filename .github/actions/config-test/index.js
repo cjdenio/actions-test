4fec9238ed85df0211e04c7be303405938a46df1
Caleb Denio <cjdenio44@gmail.com>
2020-07-22 21:56:17 -0400

    AHH 2.0

--- .github/actions/config-test/index.js
+++ .github/actions/config-test/index.js
@@ -1,8 +1,3 @@
-const actions = require('@actions/core')
-const fs = require('fs')
-const { promisify } = require('util')
-const path = require('path')
-
 async function run() {
     console.log(process.env.INPUT_TEST)
 }
